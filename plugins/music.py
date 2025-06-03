f349f0edb5728e5ffc5690924ae701631ebd0e9a
Rymer <amartyaanand2007@gmail.com>
2025-06-03 17:15:59 +0530

    created music object
    organized a little

--- plugins/music.py
+++ plugins/music.py
@@ -10,32 +10,56 @@ from pytgcalls.types import VideoQuality
 from config import PREFIXES, AUTH_USERS
 from pytgcalls import exceptions
 
-player = PyTgCalls(app)
+class Stream:
+    def __init__(self, app : Client):
+        self.player = PyTgCalls(app)
+        self.running = False
 
-running = False
+    async def start(self):
+        if not self.running:
+            await self.player.start()
+            self.running = True
 
-async def video_play(chat, link):
-    await player.play(chat,
-            MediaStream(
-            link,
-            AudioQuality.HIGH,
-            VideoQuality.SD_480p,
-        ))
-    await idle()
+    async def stream(self, chat_id : int | str, link : str):
+            await self.player.play(chat_id, 
+                                MediaStream(
+                                        link,
+                                        AudioQuality.HIGH, 
+                                        VideoQuality.HD_720p
+                                    )
+                                )
+            await idle()
+
+    async def command_handler(self, message : Message):
+        action = message.command[0]
+        chat_id = message.chat.id
+        match action:
+            case "leavecall":
+                await self.player.leave_call(chat_id)
+                await message.reply("`left call...`")
+            case "pause":
+                await self.player.pause(chat_id)
+                await message.reply("`paused...`")
+            case "resume":
+                await self.player.resume(chat_id)
+                await message.reply("`resumed...`")
+            case "m":
+                await self.player.mute(chat_id)
+                await message.reply("`muted...`")
+            case "um":
+                await self.player.unmute(chat_id)
+                await message.reply("`unmuted...`")
+            case _:
+                await message.reply("`Invalid media action`")
+
+streamer = Stream(app)
 
 @Client.on_message(filters.command("play", prefixes= PREFIXES) & filters.user(AUTH_USERS))
-async def video_command(_, message: Message):
-    if len(message.command) < 2:
-        await message.reply("Usage: /play <yt url>", quote=True, parse_mode=ParseMode.DISABLED)
-        return
+async def play_command(_, message : Message):
+    await streamer.start()
     chat_id, link = message.chat.id, message.command[1]
-    global running
-    if not running:
-        await player.start()
-        running = True
     try:
-        await video_play(chat_id, link)
-
+        await streamer.stream(chat_id, link)
     except exceptions.ClientNotStarted:
         print("----Client not started yet----")
     except FileNotFoundError:
@@ -47,32 +71,10 @@ async def video_command(_, message: Message):
     except exceptions.YtDlpError:
         await message.reply("Unexpected Yt-Dlp error: check logs")
 
-        
-
 @Client.on_message(filters.command(["leavecall", "pause", "resume", "m", "um"], prefixes= PREFIXES) & filters.user(AUTH_USERS))
-async def playback_actions(_, message: Message):
-    action = message.command[0]
-    chat_id = message.chat.id
+async def command_handler(_, message : Message):
     try:
-        match action:
-            case "leavecall":
-                await player.leave_call(chat_id)
-                await message.reply("`left call...`")
-            case "pause":
-                await player.pause(chat_id)
-                await message.reply("`paused...`")
-            case "resume":
-                await player.resume(chat_id)
-                await message.reply("`resumed...`")
-            case "m":
-                await player.mute(chat_id)
-                await message.reply("`muted...`")
-            case "um":
-                await player.unmute(chat_id)
-                await message.reply("`unmuted...`")
-            case _:
-                await message.reply("`Invalid media action`")
-  
+        await streamer.command_handler(message)
     except exceptions.ClientNotStarted:
         print("--------Client not started yet--------")
     except exceptions.NotInCallError:
