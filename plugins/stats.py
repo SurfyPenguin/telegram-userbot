a3f95bfc4a2b0e484395efd927bf71543ef69b52
Rymer <amartyaanand2007@gmail.com>
2025-06-04 11:39:36 +0530

    renamed to stats
    added memory and cpu/ more stats

--- plugins/stats.py
+++ plugins/stats.py
@@ -3,8 +3,15 @@ from pyrogram.types import Message
 from config import PREFIXES, AUTH_USERS, DCS
 from helpers.ping import do_ping
 from datetime import datetime
+from psutil import cpu_percent, virtual_memory
+
 
 start_time = datetime.now()
+
+async def get_usage():
+    cpu_pc = cpu_percent()
+    memory_pc = virtual_memory().percent
+    return cpu_pc, memory_pc
 
 @Client.on_message(filters.command("alive", prefixes= PREFIXES) & filters.user(AUTH_USERS))
 async def uptime_command(_, message : Message):
@@ -12,7 +19,9 @@ async def uptime_command(_, message : Message):
 
     ping = await do_ping(DCS[message.chat.dc_id]["ip"])
 
+    cpu, memory = await get_usage()
+
     uptime = end_time.replace(microsecond=0) - start_time.replace(microsecond=0)
-    format = f"```Status\nAlive! bot is currently running...\nUptime - `{uptime}`\nPing - {ping}ms```"
+    format = f"```status\nAlive! bot is currently running...\nUptime - `{uptime}`\nPing - {ping}ms``````Resources\nCpu - {cpu}%\nMemory - {memory}%```"
 
     await message.reply(format)
